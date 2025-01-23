15723ade5a1806537a275d6b1676835360b5c0e9
Adelin Rosca <rosca.adelin@gmail.com>
2025-01-23 18:18:49 +0200

    configured es lint rules and plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,45 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: [
+			'next/core-web-vitals',
+			'next/typescript',
+			'eslint:recommended',
+			'plugin:react/recommended',
+			'plugin:react-hooks/recommended',
+			'plugin:prettier/recommended'
+		],
+		rules: {
+			'no-console': 'error',
+			'no-param-reassign': 'error',
+			'no-unused-vars': 'error',
+			'prefer-const': 'error',
+			'for-direction': 'error',
+			'no-multiple-empty-lines': ['error', { max: 1 }],
+			'react/no-array-index-key': 'warn',
+			'react/react-in-jsx-scope': 'off',
+			'react/jsx-uses-react': 'off',
+			'no-undef': 'off'
+		},
+		plugins: ['react'],
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		}
+	})
 ];
 
 export default eslintConfig;
