1636cd2260cb4e41a6186456db530e5208b6742b
Adelin Rosca <rosca.adelin@gmail.com>
2025-01-26 11:43:56 +0200

    styled components can now be rendered on the server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import GlobalStyle from './styles/GlobalStyle';
+import StyledComponentsRegistry from '@/lib/registry';
 
 export const metadata: Metadata = {
 	title: 'Weather App',
@@ -15,25 +16,27 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body>
-				<GlobalStyle />
-				<div
-					style={{
-						width: '100%',
-						height: '80px',
-						backgroundColor: 'blue'
-					}}
-				>
-					<Link href='/' style={{ color: 'black' }}>
-						Home
-					</Link>
-					<Link href='/London' style={{ color: 'black' }}>
-						London
-					</Link>
-					<Link href='/Paris' style={{ color: 'black' }}>
-						Paris
-					</Link>
-				</div>
-				{children}
+				<StyledComponentsRegistry>
+					<GlobalStyle />
+					<div
+						style={{
+							width: '100%',
+							height: '80px',
+							backgroundColor: 'blue'
+						}}
+					>
+						<Link href='/' style={{ color: 'black' }}>
+							Home
+						</Link>
+						<Link href='/London' style={{ color: 'black' }}>
+							London
+						</Link>
+						<Link href='/Paris' style={{ color: 'black' }}>
+							Paris
+						</Link>
+					</div>
+					{children}
+				</StyledComponentsRegistry>
 			</body>
 		</html>
 	);
