be38d48b167c2ec4e080e9584df9da93afa079e6
Adelin Rosca <rosca.adelin@gmail.com>
2025-02-09 15:28:20 +0200

    created daily forecast component

---
+++ src/components/cityComponents/DailyForecastPanel/index.tsx
@@ -0,0 +1,44 @@
+import { Icon } from '@iconify/react';
+import { Card, Flex, Text, Span } from '@/components/UI/globals';
+import { getDay } from '@/utils/convertTime';
+import { handleUnitType } from '@/utils/handleUnitType';
+import { ParsedDailyForecast } from '@/utils/types/WeatherData';
+
+interface DailyForecastProps {
+	dailyForecast: ParsedDailyForecast[];
+}
+
+const DailyForecastPanel = ({ dailyForecast }: DailyForecastProps) => {
+	return (
+		<Card $height='100%'>
+			<Flex $column $justify='space-between' $width='100%' $height='100%'>
+				<Text $weight='700'>7-Day Forecast</Text>
+				<Flex $column $width='100%' $gap='24px'>
+					{dailyForecast.map((el) => (
+						<Flex
+							key={el.time}
+							$delimiter
+							$justify='space-between'
+							$align='center'
+							$width='100%'
+						>
+							<Text $width='25%'>{getDay(el.time)}</Text>
+							<Flex $justify='space-evenly' $align='center' $width='50%'>
+								<Icon icon={el.weather.icon} width='64' height='64' />
+								<Text $weight='700'>{el.weather.main}</Text>
+							</Flex>
+							<Text $align='end' $width='25%'>
+								<Span>{handleUnitType(el.temp.minTemp, 'temperature')}</Span>/
+								<Span $weight='700'>
+									{handleUnitType(el.temp.maxTemp, 'temperature')}
+								</Span>
+							</Text>
+						</Flex>
+					))}
+				</Flex>
+			</Flex>
+		</Card>
+	);
+};
+
+export default DailyForecastPanel;
